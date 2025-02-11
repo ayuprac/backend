4c7b5d53190e79c5b84231b6e15596b4fdaadb7f
Ayush k <ayushkhorgade2002@gmail.com>
2025-02-11 20:28:50 +0530

    folder added

---
+++ index.js
@@ -0,0 +1,18 @@
+const app = require('express')();
+const PORT = 8080;
+
+app.listen(
+    PORT,
+    ()=> console.log(`Server running on ${PORT}`)
+);
+
+//app.get('/fruits',(req,res)=>{
+//    res.send("Mango and Banana")
+//});
+
+app.get('/fruits',(req,res)=>{
+    res.send({
+        apple:2,
+        banana: 10
+    })
+});
